aa420e3a106abc1bd0614e7051ceb01d6fdd3216
MinWooPark-dotcom <positive1attraction@gmail.com>
2025-04-12 17:54:41 +0900

    feat(terraform): add dynamic cluster name and region variables in terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,62 @@
+variable "region" {
+  description = "AWS region to deploy EKS"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "vpc_id" {
+  description = "VPC ID for the EKS cluster"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "Subnets for EKS"
+  type        = list(string)
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Enable public access to EKS endpoint"
+  type        = bool
+  default     = true
+}
+
+variable "cluster_endpoint_private_access" {
+  description = "Enable private access to EKS endpoint"
+  type        = bool
+  default     = false
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in node group"
+  type        = number
+  default     = 3
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in node group"
+  type        = number
+  default     = 1
+}
+
+variable "node_instance_types" {
+  description = "EC2 instance types for EKS nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
